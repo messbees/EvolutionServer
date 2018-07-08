0971f57af8c1e63ba136ed666c9a47a32072d385
Anton Betsun <messbees@gmail.com>
2018-07-09 02:19:33 +0300

    add to_json and creature's card

--- creature.py
+++ creature.py
@@ -1,7 +1,11 @@
+import player
+import ability
+
 class Creature:
     id = 0
-    def __init__(self, owner):
+    def __init__(self, owner, card):
         Creature.id = 1
+        self.card = card
         self.id = 1
         self.hunger = 1
         self.food = 0
@@ -9,3 +13,15 @@ class Creature:
         self.abilities = []
         serf.owner = owner
         print("{} has spawned a new creature (ID: {})".format(self.owner, self.id))
+
+    def json(self):
+        json = {}
+        json["id"] = self.id
+        json["hunger"] = self.hunger
+        json["food"] = self.food
+        json["fat"] = self.fat
+        json["owner"] = self.owner
+        json["abilities"] = {}
+        for ability in self.abilities:
+            json["abilities"] = ability.id
+        json["card"] = card.id
