c5e89ee07989edf426d4aeba4fad03f1e96de8fe
Anton Betsun <messbees@gmail.com>
2018-07-08 23:33:40 +0300

    add basic ability class

---
+++ ability.py
@@ -0,0 +1,20 @@
+class Ability:
+
+    def __init__(self, name, description, type, action, owner):
+        self.name = name
+        self.description = description
+        self.type = type
+        self.action = action
+
+    def set_owner(self, owner):
+        self.owner = owner
+
+    def apply(self):
+        self.action()
+
+
+def initialyze():
+    abilities = []
+    abilities.append(Ability("Predator", "This allows you to eat other creatures", "abilty"))
+    abilities.append(Ability("Fat", "This allows you to store extra food for next turns", "passive"))
+    return abilities
