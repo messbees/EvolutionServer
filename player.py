8a591b3bde12c8af0bcb95995c351a1f3f1ba04d
Anton Betsun <messbees@gmail.com>
2018-07-09 02:22:24 +0300

    add json

--- player.py
+++ player.py
@@ -1,3 +1,6 @@
+import creature
+import ability
+
 class Player:
     def __init__(self, name, deck):
         self.name = name
@@ -5,4 +8,13 @@ class Player:
         self.cards = []
         for index in range(0, 5):
             self.cards.append(deck.get_card())
-            
+
+    def json(self):
+        json = {}
+        json["name"] = self.name
+        json["creatures"] = {}
+        for creature in self.creatures:
+            json["creatures"][creature.id] = creature.json()
+        json["cards"] = {}
+        for card in self.cards:
+            json["cards"].append(card.id)
