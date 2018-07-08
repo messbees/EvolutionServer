ab66bf4221e6da30dca64d4a22ac384e650f3a64
Anton Betsun <messbees@gmail.com>
2018-07-08 23:38:19 +0300

    add deck class

---
+++ deck.py
@@ -0,0 +1,13 @@
+import ability
+import random
+
+class Deck:
+    def __init__(self):
+        self.cards = ability.initialyze()
+        print("Created deck with {} cards.".format(self.cards.count))
+
+    def get_card():
+        card = random.choise(self.cards)
+        self.cards.pop(card)
+        print("Deck now have {} cards left.".format(self.cards.count))
+        return card
