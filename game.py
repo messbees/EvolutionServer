34db6900cf1a533398b23a7070f3580ddb396abb
Anton Betsun <messbees@gmail.com>
2018-07-09 02:27:36 +0300

    add json

--- game.py
+++ game.py
@@ -8,23 +8,29 @@ class Game:
     def __init__(self, name, players, deck):
         self.name = name
         self.id = random.randrange(1000, 9999)
-        self.deck = deck
-        self.players = players
         self.round = 1
         self.stage = "evolution"
         first = random.choise(players)
         self.turn = first.name
+        self.dice = 0
+        self.food = 0
+        self.players = players
+        self.deck = deck
 
     def json(self):
         json = {}
-        json["players"] = {}
-        json["deck"] = {}
         json["game"] = {}
         json["game"]["name"] = self.name
         json["game"]["id"] = self.id
         json["game"]["round"] = self.round
         json["game"]["stage"] = self.stage
         json["game"]["turn"] = self.turn
+        json["game"]["dice"] = self.dice
+        json["game"]["food"] = self.food
+        json["players"] = {}
         for player in self.players:
             json["players"][player.name] = player.json()
+        json["deck"] = {}
+        for card in self.deck:
+            json["deck"].append(card.id)
         return json
