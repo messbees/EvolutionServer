ba470513ccee439c6b114cfe6e37b0b7e0a1364a
Anton Betsun <messbees@gmail.com>
2018-07-09 02:18:03 +0300

    add ability id

--- ability.py
+++ ability.py
@@ -1,9 +1,10 @@
 class Ability:
 
-    def __init__(self, name, type, action, owner):
+    def __init__(self, name, type, action, id):
         self.name = name
         self.type = type
         self.action = action
+        self.id = id
 
     def set_owner(self, owner):
         self.owner = owner
@@ -14,26 +15,26 @@ class Ability:
 
 def initialyze():
     abilities = []
-    abilities.append(Ability("Predator", "This allows you to eat other creatures", "abilty"))
-    abilities.append(Ability("Fat", "This allows you to store extra food for next turns", "passive"))
-    abilities.append(Ability("Predator", "This allows you to eat other creatures", "abilty"))
-    abilities.append(Ability("Fat", "This allows you to store extra food for next turns", "passive"))
-    abilities.append(Ability("Predator", "This allows you to eat other creatures", "abilty"))
-    abilities.append(Ability("Fat", "This allows you to store extra food for next turns", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 1", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 2", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 3", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 4", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 5", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 6", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 7", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 8", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 1", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 2", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 3", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 4", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 5", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 6", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 7", "passive"))
-    abilities.append(Ability("Dummy", "This is dummy ability number 8", "passive"))
+    abilities.append(Ability("Predator", "This allows you to eat other creatures", "abilty", 1))
+    abilities.append(Ability("Fat", "This allows you to store extra food for next turns", "passive", 2))
+    abilities.append(Ability("Predator", "This allows you to eat other creatures", "abilty", 1))
+    abilities.append(Ability("Fat", "This allows you to store extra food for next turns", "passive", 2))
+    abilities.append(Ability("Predator", "This allows you to eat other creatures", "abilty", 1))
+    abilities.append(Ability("Fat", "This allows you to store extra food for next turns", "passive", 2))
+    abilities.append(Ability("Dummy", "This is dummy ability number 1", "passive", 3))
+    abilities.append(Ability("Dummy", "This is dummy ability number 2", "passive", 4))
+    abilities.append(Ability("Dummy", "This is dummy ability number 3", "passive", 5))
+    abilities.append(Ability("Dummy", "This is dummy ability number 4", "passive", 6))
+    abilities.append(Ability("Dummy", "This is dummy ability number 5", "passive", 7))
+    abilities.append(Ability("Dummy", "This is dummy ability number 6", "passive", 8))
+    abilities.append(Ability("Dummy", "This is dummy ability number 7", "passive", 9))
+    abilities.append(Ability("Dummy", "This is dummy ability number 8", "passive", 10))
+    abilities.append(Ability("Dummy", "This is dummy ability number 1", "passive", 3))
+    abilities.append(Ability("Dummy", "This is dummy ability number 2", "passive", 4))
+    abilities.append(Ability("Dummy", "This is dummy ability number 3", "passive", 5))
+    abilities.append(Ability("Dummy", "This is dummy ability number 4", "passive", 6))
+    abilities.append(Ability("Dummy", "This is dummy ability number 5", "passive", 7))
+    abilities.append(Ability("Dummy", "This is dummy ability number 6", "passive", 8))
+    abilities.append(Ability("Dummy", "This is dummy ability number 7", "passive", 9))
+    abilities.append(Ability("Dummy", "This is dummy ability number 8", "passive", 10))
     return abilities
