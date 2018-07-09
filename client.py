badeb7187adc73e89ad37f35e22a60415b931fb8
Anton Betsun <messbees@gmail.com>
2018-07-10 00:23:31 +0300

    hotfix

--- client.py
+++ client.py
@@ -37,14 +37,14 @@ def connect_to_room(args):
 def main(prog_name=os.path.basename(sys.argv[0]), args=None):
 	if args is None:
 		args = sys.argv[1:]
-	parser = create_parser(prog_name)
+	parser = parser_helper.create_parser(prog_name)
 	args = parser.parse_args(args)
 
 	if args.verbose is None:
 		verbose_level = 0
 	else:
 		verbose_level = args.verbose
-	setup_loggers(verbose_level=verbose_level)
+	parser_helper.setup_loggers(verbose_level=verbose_level)
 
 	if not args.command:
 		parser.print_help()
