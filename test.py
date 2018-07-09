eef2f04010dd1753dad4e7311c8aa66066154128
Anton Betsun <anton.betsun@sharekey.com>
2018-07-09 02:25:29 +0000

    fix url

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def main():
     json["room_new"]["player"] = "messbees"
     print(json)
     print("Sending your {} request...".format(json["action"]))
-    print(requests.post('http://localhost:8888', json=json))
+    print(requests.post('http://159.100.247.47:8888', json=json))
 
 if __name__ == "__main__":
     main()
