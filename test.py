f225f73b18eade6d4aed8070a5bd418e01ba1595
Anton Betsun <messbees@gmail.com>
2018-07-09 04:54:56 +0300

    add json category definition

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import requests
 def main():
     json = {}
     json["action"] = "ROOM_NEW"
+    json["room_new"] = {}
     json["room_new"]["game"] = "game"
     json["room_new"]["player"] = "messbees"
     print("Sending your {} request...".format(json[action]))
