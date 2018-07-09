0ad991ca14d664e7b0a77169f2527f56ec1a0e6c
Anton Betsun <messbees@gmail.com>
2018-07-09 04:56:18 +0300

    fix missing quotes

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def main():
     json["room_new"] = {}
     json["room_new"]["game"] = "game"
     json["room_new"]["player"] = "messbees"
-    print("Sending your {} request...".format(json[action]))
+    print("Sending your {} request...".format(json["action"]))
     print(requests.post('159.100.247.47:8888', json=json))
 
 if __name__ == "__main__":
