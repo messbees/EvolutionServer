7d259c6394d4ea6a73afb88d1cd57083e4aadd04
Anton Betsun <messbees@gmail.com>
2018-07-10 02:56:32 +0300

    fix missing self in def

--- deck.py
+++ deck.py
@@ -6,7 +6,7 @@ class Deck:
         self.cards = ability.initialyze()
         print("Created deck with {} cards.".format(self.cards.count))
 
-    def get_card():
+    def get_card(self):
         card = random.choise(self.cards)
         self.cards.pop(card)
         print("Deck now have {} cards left.".format(self.cards.count))
