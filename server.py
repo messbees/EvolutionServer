a38f6b47956bc6c61b4af75f6918cf28d957d436
Anton Betsun <anton.betsun@sharekey.com>
2018-07-10 16:50:50 +0300

    big code refactoring and do_evolution draft

--- server.py
+++ server.py
@@ -20,21 +20,73 @@ class Server:
         if (os.path.isfile("games/{}.json".format(id))):
             f = open('games/{}.json'.format(id))
             game = json.loads(f.read())
+            return game
         else:
             return None
 
-    def new_game(self, name, players, deck):
-        game = Game(name, players, deck)
-        return game
+    def new_game(self, game, players, deck):
+        g = Game(game, players, deck)
+        return g
+
+    def new_room(self, game, admin):
+        if (os.path.isfile("rooms/{}.json".format(game))):
+            return False
+        else:
+            room = Room(game, admin)
+            return True
+
+    def join_room(self, game, new_player):
+        if (os.path.isfile("rooms/{}.json".format(game))):
+            f = open('rooms/{}.json'.format(game))
+            room = json.loads(f.read())
+            name = room["name"]
+            players = []
+            for player in room["players"]:
+                players.append(player)
+            admin = room["admin"]
+            updated = Room(name, admin)
+            for player in players:
+                if not (updated.connect(player)):
+                    return 'WRONG_USER'
+            if not (updated.connect(new_player)):
+                return 'WRONG_USER
+            updated.save()
+            return 'JOINED'
+        else:
+            return 'WRONG_ROOM'
+
+    def begin_game(self, game, admin):
+        if not (os.path.isfile("rooms/{}.json".format(game))):
+            return 'WRONG_ROOM'
+        f = open('rooms/{}.json'.format(game))
+        room = json.loads(f.read())
+        if not (room["admin"] == admin):
+            return 'NOT_ADMIN'
+        deck = Deck()
+        players = []
+        for player in room["players"]:
+            players.append(Player(player, deck))
+        print('Creating game...')
+        g = game_server.new_game(game, players, deck)
+        if not (os.path.isfile("games/{}.json".format(g.id))):
+            g.save()
+            print("Game {} begins!".format(g.id))
+            return 'BEGIN'
+        else:
+            print('Game with same id already exists.')
+            return 'WRONG_ID'
 
     def do_evolution(self, game, player, creature, card):
-        if (game.stage == "evolution" and game.turn == player_name):
-            if (game.do_evolution(player, creature, card)):
-                return true
-        return false
+        if (game["stage"] == "evolution"):
+            for p in game["players"]:
+                if (p["name"] == player):
+                    if not (game["turn"] == player):
+                        return 'NOT_YOUR_TURN'
+                    if (creature == 999):
+            return 'WRONG_USER'
+        else:
+            return 'WRONG_STAGE'
 
-    def save_game(self, game):
-        print("")
 
 game_server = Server()
 
@@ -42,92 +94,16 @@ class RequestHandler(BaseHTTPRequestHandler):
     def set_game_server(self, server):
         self.game_server = server
 
-    def do_POST(self):
-
-        print("POST request!")
+    def do_GET(self):
         self.data_string = self.rfile.read(int(self.headers['Content-Length']))
         data = json.loads(self.data_string)
         action = data["action"]
 
-        #self.send_header('Content-type', 'application/json')
-        # calls after creating new room
-        if (action == "ROOM_NEW"):
-            game_name = data["room_new"]["game"]
-            player_name = data["room_new"]["player"]
-            if (os.path.isfile("rooms/{}.json".format(game_name))):
-                self.send_response(409)
-                self.end_headers()
-            else:
-                room = Room(game_name, player_name)
-                self.send_response(200)
-                self.end_headers()
-
-        # calls after creating new room, when connecting to existing room, or while updating current room
-        if (action == "ROOM_CONNECT"):
-            game_name = data["room_connect"]["game"]
-            player_name = data["room_connect"]["player"]
-            if (os.path.isfile("rooms/{}.json".format(game_name))):
-                f = open('rooms/{}.json'.format(game_name))
-                room = json.loads(f.read())
-                name = room["name"]
-                players = []
-                for player in room["players"]:
-                    players.append(player)
-                print("----players: {}".format(players))
-                admin = room["admin"]
-                updated = Room(name, admin)
-                for player in players:
-                    if not (updated.connect(player)):
-                        self.send_response(409)
-                        self.end_headers()
-                        return
-                if not (updated.connect(player_name)):
-                    self.send_response(409)
-                    self.end_headers()
-                    return
-                updated.save()
-                self.send_response(200)
-                self.end_headers()
-            else:
-                self.send_response(404)
-                self.end_headers()
-
-        # calls after beginning the game in room by room admin
-        if (action == "ROOM_START"):
-            name = data["room_start"]["game"]
-            admin = data["room_start"]["player"]
-            if not (os.path.isfile("rooms/{}.json".format(name))):
-                self.send_response(404)
-                self.end_headers()
-                return
-            f = open('rooms/{}.json'.format(name))
-            room = json.loads(f.read())
-            if not (room["admin"] == admin):
-                self.send_response(403)
-                self.end_headers()
-                return
-            deck = Deck()
-            players = []
-            for player in room["players"]:
-                players.append(Player(player, deck))
-            print('Creating game...')
-            game = game_server.new_game(name, players, deck)
-            if not (os.path.isfile("games/{}.json".format(game.id))):
-                game.save()
-                print("Game {} begins!".format(game.id))
-                self.send_response(200)
-                self.end_headers()
-                return
-            else:
-                print('Game with same id already exists.')
-                self.send_response(500)
-                self.end_headers()
-                return
-
+        # calls after trying to fetch room state
         if (action == "ROOM_UPDATE"):
-            name = data["room_update"]["game"]
+            game = data["room_update"]["game"]
             player = data["room_update"]["player"]
-            if not (os.path.isfile("rooms/{}.json".format(name))):
+            if not (os.path.isfile("rooms/{}.json".format(game))):
                 for file in os.listdir("games/"):
                     if file.endswith(".json"):
                         f = open(file)
@@ -163,43 +139,103 @@ class RequestHandler(BaseHTTPRequestHandler):
                 return
 
 
-
-        # calls after trying to fetch game
-        if (action == "CONNECT"):
-            game_name = data["connect"]["game"]
-            player_name = data["connect"]["player"]
-            for game in game_server.games:
-                for player in game.players:
-                    if (player_name == player.name):
-                        f = open('games/{}.json'.format(game.id))
-                        self.wfile.write(f.read())
-                        return
-
-            f = open('games/null.json')
-            self.wfile.write(f.read())
-
-        if (action == "EVOLUTION"):
-            game_id = data["evolution"]["game_id"]
-            player_name = data["evolution"]["player"]
-            creature = data["evolution"]["creature"]
-            card = data["evolution"]["card"]
-            game = game_server.load_game(game_id)
-            if (game == None):
+        # calls after trying to fetch game state
+        if (action == "UPDATE"):
+            game = data["update"]["game"]
+            player = data["update"]["player"]
+            if not (os.path.isfile("games/{}.json".format(game))):
                 self.send_response(404)
                 self.end_headers()
                 return
-            player = None
+            f = open('games/{}.json'.format(game))
+            g = json.loads(f.read())
+            for p in g.players:
+                if (player == p.name):
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/json")
+                    self.end_headers()
+                    self.wfile.write(f.read())
+                    return
+            self.send_response(403)
+            self.end_headers()
+
+
+    def do_POST(self):
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+        data = json.loads(self.data_string)
+        action = data["action"]
+
+        # calls after creating new room
+        if (action == "ROOM_NEW"):
+            game = data["room_new"]["game"]
+            player = data["room_new"]["player"]
+            if (game_server.new_room(game, player)):
+                self.send_response(200)
+                self.end_headers()
+            else:
+                self.send_response(409)
+                self.end_headers()
+
+        # calls after creating new room, when connecting to existing room, or while updating current room
+        if (action == "ROOM_CONNECT"):
+            game = data["room_connect"]["game"]
+            player = data["room_connect"]["player"]
+            status = game_server.join_room(game, player)
+            if (status == 'WRONG_USER'):
+                self.send_response(409)
+                self.end_headers()
+            elif(status == 'WRONG_ROOM'):
+                self.send_response(404)
+                self.end_headers()
+            elif(status == 'JOINED'):
+                self.send_response(200)
+                self.end_headers()
+
+        # calls after beginning the game in room by room admin
+        if (action == "ROOM_START"):
+            game = data["room_start"]["game"]
+            admin = data["room_start"]["player"]
+            status = game_server.begin_game(game, admin)
+            if (status == 'WRONG_ROOM'):
+                self.send_response(404)
+                self.end_headers()
+            elif (status == 'NOT_ADMIN'):
+                self.send_response(403)
+                self.end_headers()
+            elif (status == 'WRONG_ID'):
+                self.send_response(500)
+                self.end_headers()
+            elif (status == 'BEGIN'):
+                self.send_response(200)
+                self.end_headers()
+
+
+        if (action == "EVOLUTION"):
+            game = data["evolution"]["game_id"]
+            player = data["evolution"]["player"]
+            creature = data["evolution"]["creature"]
+            card = data["evolution"]["card"]
+            g = game_server.load_game(game)
+            if (g == None):
+                self.send_response(404)
+                self.end_headers()
+                return
+            status = game_server.do_evolution(g, player, creature, card)
+
+            elif (status == 'WRONG_USER'):
+                self.send_response(403)
+                self.end_headers()
+                return
             for p in game.players:
-                if (p.name == player_name):
+                if (p.name == player):
                     player = p
             if (game_server.do_evolution(game, player, creature, card)):
                 self.send_response(200)
                 self.end_headers()
                 return
-            else:
-                self.send_response(403)
-                self.end_headers()
-                return
+            self.send_response(403)
+            self.end_headers()
+
         return
 
 if __name__ == "__main__":
