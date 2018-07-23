04314a50388e63617939e61a5d6f601d3ce6ece7
messbees <messbees@gmail.com>
2018-07-23 12:05:35 +0300

    remove debug messages

--- deck.py
+++ deck.py
@@ -4,10 +4,8 @@ import random
 class Deck:
     def __init__(self):
         self.cards = ability.initialyze()
-        print("Created deck with {} cards.".format(len(self.cards)))
 
     def get_card(self):
         card = random.choice(self.cards)
         self.cards.remove(card)
-        print("Deck now have {} cards left.".format(len(self.cards)))
         return card
