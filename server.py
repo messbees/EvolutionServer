b20c1594057b89bbd4fffc2a1c5c17be1322fd34
messbees <messbees@gmail.com>
2018-07-25 02:05:17 +0300

    add debug message

--- server.py
+++ server.py
@@ -193,6 +193,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                     self.send_response(200)
                     self.send_header("Content-type", "application/json")
                     self.end_headers()
+                    LOGGER.debug(game.json)
                     self.wfile.write(game.json)
                     return
             LOGGER.warn("Player is not in this game.")
