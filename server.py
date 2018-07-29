c9cb3408c971842e2cbf56018bdb4655c5b15e11
messbees <messbees@gmail.com>
2018-07-29 15:15:19 +0300

    a

--- server.py
+++ server.py
@@ -123,7 +123,7 @@ class Server:
             return 'WRONG_STAGE'
         for p in game.players:
             if (p.name == player):
-                for c in p.creations:
+                for c in p.creatures:
                     print(c.json())
                 if not (game.turn == player):
                     LOGGER.warn("It is not {}'s turn!".format(player))
